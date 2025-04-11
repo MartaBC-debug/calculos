1eacc8d75fa09903ebbd87a59be9bcfaf8a164f6
Marta Coelho <148581350+MartaBC-debug@users.noreply.github.com>
2025-04-11 23:32:19 +0100

    Tornar a classe Aritmetica numa classe static

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -9,12 +9,12 @@ namespace Calculos
     /// <summary>
     /// Implementa operações aritméticas
     /// </summary>
-    internal class Aritmetica
+    internal static class Aritmetica
     {
         /// <summary>
         /// Tipo de conversão de Temperatura a executar.
         /// </summary>
-        public enum ConversaoTemperatura
+        public static enum ConversaoTemperatura
         {
             Nulo=0,
             CelsiusFahrenheit,
@@ -24,7 +24,7 @@ namespace Calculos
         /// Operação soma
         /// </summary>
         /// <returns>Retorna a soma de dois números</returns>
-        public int Somar(int x, int y)
+        public static int Somar(int x, int y)
         {
             return x + y;
         }
@@ -32,11 +32,11 @@ namespace Calculos
         /// Operação de subtração
         /// </summary>
         /// <returns>Retorna o resultado da substração de dois números</returns>
-        public int Subtrair(int x, int y) 
+        public static int Subtrair(int x, int y) 
         {
             return x - y;
         }
-        public double ConverterTemperatura(ConversaoTemperatura conversao, double temperatura)
+        public static double ConverterTemperatura(ConversaoTemperatura conversao, double temperatura)
         {
             if (conversao == ConversaoTemperatura.CelsiusFahrenheit)
             {
